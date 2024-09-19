b47db64a47418bb6f33131a2d621b3074e239341
AdrianMGF <gordillofernandezadrianmaria@gmail.com>
2024-09-19 13:47:29 +0200

    Todo

---
+++ Assets/Scripts/Cambiohab.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cambiohab : MonoBehaviour
+{
+    public GameObject origen; 
+    public GameObject destino;  
+    
+    public void cambio(){
+
+        origen.SetActive(false);
+        destino.SetActive(true);
+
+    }
+    
+    
+    
+    
+    
+    
+    
+    }
+    
---
+++ Assets/Scripts/Inicio.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Inicio : MonoBehaviour
+{
+    public void casa1(){
+        SceneManager.LoadScene("Casa1", LoadSceneMode.Single);
+    }
+    public void casa2(){SceneManager.LoadScene("Casa2", LoadSceneMode.Single);}
+    public void casa3(){SceneManager.LoadScene("Casa3", LoadSceneMode.Single);}
+    public void casa4(){SceneManager.LoadScene("Casa4", LoadSceneMode.Single);}
+    public void casa5(){SceneManager.LoadScene("Casa5", LoadSceneMode.Single);}
+
+    
+}
